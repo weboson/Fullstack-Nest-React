ce15fa82d4271a6f56d752859cd67321f886a459
Rishat <virisound@gmail.com>
2023-07-30 00:07:53 +0500

    Installed '@nestjs/config', added .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,16 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule } from '@nestjs/config'; // для подключения .env (bd_port, bd_password...)
 
 @Module({
-  imports: [AuthModule, CategoryModule, TransactionModule, UserModule],
+  imports: [
+    AuthModule, 
+    CategoryModule, 
+    TransactionModule, 
+    UserModule, 
+    ConfigModule.forRoot({isGlobal: true}) // подключение .env - forRoot используется для корневого модуля (то есть этого файла)
+  ], 
   controllers: [AppController],
   providers: [AppService],
 })
