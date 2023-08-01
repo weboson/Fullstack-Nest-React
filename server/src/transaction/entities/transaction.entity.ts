832c3eda840bd22a7339129dbebac1a6811fd8fc
Rishat <virisound@gmail.com>
2023-08-02 02:30:04 +0500

    Creating tables for entities: user, category, transaction

--- server/src/transaction/entities/transaction.entity.ts
+++ server/src/transaction/entities/transaction.entity.ts
@@ -1,1 +1,36 @@
-export class Transaction {}
+import { Category } from 'src/category/entities/category.entity';
+//! Таблица для БД, сущности Transaction (доход/расход)
+import { User } from "src/user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Transaction {
+
+    @PrimaryGeneratedColumn({ name: 'transaction_id' })
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column({ nullable: true }) // позволенное значение, т.е. может быть null 
+    type: string; // тип: доход или расход
+
+    // связь
+    @ManyToOne(() => User, (user) => user.transactions)
+    @JoinColumn({ name: 'user_id' }) // объединить  в колонку "user_id"
+    user: User;
+
+    // связь
+    @ManyToOne(() => Category, (category) => category.transactions)
+    @JoinColumn({ name: 'category_id' })
+    category: Category;
+
+    @Column()
+    amount: number;
+
+    @CreateDateColumn() 
+    createdAt: Date;
+
+    @UpdateDateColumn() 
+    updatedAt: Date;
+}
